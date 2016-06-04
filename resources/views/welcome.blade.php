c6eec8efc6e6b5b6d80dbef6412a907cb1841359
Behnam Baharmand <behnam@baharmand.com>
2016-06-04 15:58:05 +0430

    Add Bootstrap through npm and Elixir

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Persian Paste Bin</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,10 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 <br>~ Behnam Baharmand!</div>
+                <div class="title">Persian Paste Bin.</div>
+                <h2>
+                  Behnam Baharmand!
+                </h2>
             </div>
         </div>
     </body>
