4d366aa6e1f2ef1b0db03660327319f4f4ed35b1
Behnam Baharmand <behnam@baharmand.com>
2016-06-05 15:13:11 +0430

    Update index content and text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,46 +3,69 @@
     <head>
         <title>Persian Paste Bin</title>
 
+        <link href="css/app.css" rel="stylesheet">
+
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
-            html, body {
-                height: 100%;
-            }
+          body {
+            padding-top: 20px;
+            padding-bottom: 20px;
+          }
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+          .navbar {
+            margin-bottom: 20px;
+          }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
+        .footer {
+          position: absolute;
+          padding: 7px;
+          bottom: 0;
+          width: 100%;
+          /* Set the fixed height of the footer here */
+          height: 50px;
+          background-color: #f5f5f5;
+        }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Persian Paste Bin.</div>
-                <h2>
-                  Behnam Baharmand!
-                </h2>
-            </div>
-        </div>
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">PasteBin.</a>
+      </div>
+      </div>
+      </nav>
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+      <div class="container">
+      <h1>PasteBin</h1>
+      <p>PasteBin is yet another simple tool that enables you to save your text snippets and share them.</p>
+      </div>
+      </div>
+
+      <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        Content
+      </div>
+
+      </div> <!-- /container -->
+
+      <footer class="footer">
+      <div class="container">
+        <p class="text-muted">&copy; 2016 Behinweb Inc.</p>
+      </div>
+      </footer>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
