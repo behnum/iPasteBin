d906362fbf179fee8948c7abfcdfb676e2fc998a
Behnam Baharmand <behnam@baharmand.com>
2016-06-07 18:11:05 +0430

    Add includes in views and change name to iPasteBin

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+<div class="container">
+  <p class="text-muted">&copy; 2016 iPasteBin, Behinweb Inc. 32874927349283749284@$%@#%$#@$%</p>
+</div>
+</footer>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,25 +1,12 @@
 @extends('app')
 @section('content')
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-<div class="container">
-<div class="navbar-header">
-  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-    <span class="sr-only">Toggle navigation</span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-    <span class="icon-bar"></span>
-  </button>
-  <a class="navbar-brand" href="#">PasteBin.</a>
-</div>
-</div>
-</nav>
+@include('includes.nav')
 
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
 <div class="container">
-<h1>Abous PasteBin.</h1>
-<p>PasteBin is yet another simple tool that enables you to save your text snippets and share them.</p>
+<h1>Abous iPasteBin.</h1>
+<p>iPasteBin is yet another simple tool that enables you to save your text snippets and share them.</p>
 </div>
 </div>
 
@@ -31,10 +18,5 @@
 
 </div> <!-- /container -->
 
-<footer class="footer">
-<div class="container">
-  <p class="text-muted">&copy; 2016 Behinweb Inc.</p>
-</div>
-</footer>
-
+@include('includes.footer')
 @endsection
